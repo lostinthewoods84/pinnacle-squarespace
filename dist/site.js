2594794d61b7764f5d10c10467b23ec92ee32ea6
lostinthewoods84 <aaron.gomo@gmail.com>
2026-01-07 00:10:08 -0500

    Ordering the loading of files

--- dist/site.js
+++ dist/site.js
@@ -1,28 +1,4 @@
-// src/js/site.js
-(function () {
-
-    function mount(targetSelector, html) {
-        const target = document.querySelector(targetSelector);  // Changed to querySelector
-        if (!target) return false;
-        target.innerHTML = html;
-        return true;
-    }
-
-    function renderHome() {
-        const templates = window.PINNACLE_TEMPLATES || {};
-
-        // Home page placeholder in Squarespace should be: <div class="pinnacle-home"></div>
-        if (templates.home) {
-            mount(".pinnacle-home", templates.home());  // Changed to class selector
-        }
-    }
-
-    if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", renderHome, { once: true });
-    } else {
-        renderHome();
-    }
-})();// src/js/templates.js
+// src/js/templates.js
 window.PINNACLE_TEMPLATES = window.PINNACLE_TEMPLATES || {};
 
 window.PINNACLE_TEMPLATES.home = function () {
@@ -191,4 +167,28 @@ window.PINNACLE_TEMPLATES.home = function () {
 
 </div>
 `;
-};
+};// src/js/site.js
+(function () {
+
+    function mount(targetSelector, html) {
+        const target = document.querySelector(targetSelector);  // Changed to querySelector
+        if (!target) return false;
+        target.innerHTML = html;
+        return true;
+    }
+
+    function renderHome() {
+        const templates = window.PINNACLE_TEMPLATES || {};
+
+        // Home page placeholder in Squarespace should be: <div class="pinnacle-home"></div>
+        if (templates.home) {
+            mount(".pinnacle-home", templates.home());  // Changed to class selector
+        }
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", renderHome, { once: true });
+    } else {
+        renderHome();
+    }
+})();
